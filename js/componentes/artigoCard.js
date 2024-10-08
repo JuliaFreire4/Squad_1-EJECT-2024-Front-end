1c72b4532237ffe91e7d2488cc1abed727a77d44
JuliaFreire4 <julia.23js83@gmail.com>
2024-10-08 08:26:46 -0300

    adicionado estrutura js lista

--- js/componentes/artigoCard.js
+++ js/componentes/artigoCard.js
@@ -12,3 +12,28 @@ const artigos = [
         imagem:'img/home/img_artigo3.jpeg'
      }
 ]
+
+const artigo_container = document.getElementById('artigo_container')
+
+
+artigos.forEach(artigo=> {
+    const artigoContent = document.createElement('div');
+    artigoContent.classList.add('artigo-content');
+
+    artigoContent.innerHTML= `
+      <div class="artigo-container" id="artigo_container">
+            <div class="card" id="artigo-card">
+                <span class="artigo-overlay">
+                    <div class="artigo-card-image">
+                        <img src="img/home/img_artigo.jpeg" alt="imagem do artigo" >
+                    </div>
+                </span>
+                <div class="descricao-content" id="descricao-artigo">
+                    <p>Lorem ipsum dolor sit amet, consectetur</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    artigo_container.appendChild(artigoContent)
+})
