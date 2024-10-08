fb666348b61a564c8d6909ba5c5eea7a99fedecc
JuliaFreire4 <julia.23js83@gmail.com>
2024-10-08 08:16:46 -0300

    adicionado constante artigo js

---
+++ js/componentes/artigoCard.js
@@ -0,0 +1,14 @@
+const artigos = [
+    {
+        descricao: "Lorem ipsum dolor sit amet, consectetur",
+        imagem:'img/home/img_artigo.jpeg'
+    },
+     {
+        descricao: "Lorem ipsum dolor sit amet, consectetur",
+        imagem:'img/home/img_artigo2.jpeg'
+    },
+     {
+        descricao: "Lorem ipsum dolor sit amet, consectetur",
+        imagem:'img/home/img_artigo3.jpeg'
+     }
+]
