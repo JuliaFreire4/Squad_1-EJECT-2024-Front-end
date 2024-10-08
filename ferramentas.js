4dfbaeb69896c3c03d38637a8b4ea8c199811047
JuliaFreire4 <julia.23js83@gmail.com>
2024-10-08 11:41:01 -0300

    adicionado iamgens ferramenta e pasta ferramenta (imagens) e adicionado objeto ferramentas js

---
+++ ferramentas.js
@@ -0,0 +1,19 @@
+const ferramentas = [
+    {
+        descricao: "Desenhos para colorir",
+        imagem:'img/home/ferramentas/ferramenta1.png',
+        link: '#'
+    },
+     {
+        descricao: "Cartões didáticos",
+        imagem:'img/home/ferramentas/ferramenta2.jpeg',
+        link: '#'
+
+    },
+     {
+        descricao: "Lorem ipsum dolor sit amet, consectetur",
+        imagem:'img/home/ferramentas/ferramenta3.jpeg',
+        link: '#'
+
+     }
+]
