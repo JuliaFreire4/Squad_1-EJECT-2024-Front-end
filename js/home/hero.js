1c87a2de597aeea206c878c5092965a5d1346d04
JuliaFreire4 <julia.23js83@gmail.com>
2024-10-10 02:11:53 -0300

    alterado nomes constantes para manter o padrao nossas ferramentas js

--- js/home/hero.js
+++ js/home/hero.js
@@ -1,8 +1,11 @@
 
 const textoCompletoHero = document.querySelector('.texto-introducao-pagina').textContent
-let primeiraParteDestaque = textoDestaque[0].textContent;
-let primeiraParteDestaque = textoDestaque[0].textContent;
-let primeiraParteDestaque = textoDestaque[0].textContent;
+
+const textoEquipeCompleto=document.getElementById('texto-equipe').textContent;
+const textoEquipeDestaque = document.querySelectorAll('#texto-equipe .text-bold-yellow-large');
+let primeiraParteDestaque_equipe = textoDestaque[0].textContent;
+let segundaParteDestaque_equipe = textoDestaque[1].textContent; 
+
 
 
 console.log(textoCompletoHero)
